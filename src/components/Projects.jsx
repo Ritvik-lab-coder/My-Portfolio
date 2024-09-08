36ed3773a7ec37912e5fe01d6f8075d2c85ecc9f
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-09-08 20:06:29 +0530

    updated project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import project1 from '../assets/project1.png'
-import project2 from '../assets/project2.jpg'
+import project2 from '../assets/project2.png'
 import project3 from '../assets/project3.jpg'
 import { motion } from 'framer-motion'
 
@@ -31,14 +31,15 @@ const Projects = () => {
                         <img src={project2} alt="" width={350} height={350} className='mb-6 rounded' />
                     </motion.div>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1.5 }} className='w-full max-w-xl lg:w-3/4'>
-                        <h6 className='mb-2 font-semibold'>Hospital Management System - HealthCheckmate</h6>
-                        <p className='mb-4 text-neutral-400'>Developed a comprehensive Hospital Management System which is designed to streamline hospital operations by providing an intuitive interface for managing patient records, appointments, doctor schedules, and more. Included schedules and information management for doctors, including specialty, availability, and patient assignments. Used JWT for authentication and authorization, bcrypt for secure hashing of user credentials and cloudinary for handling and uploading images for user and doctor profiles.</p>
+                        <h6 className='mb-2 font-semibold'> Full-Stack Job Search and Application Platform</h6>
+                        <p className='mb-4 text-neutral-400'> Developed a full-stack job search and application platform enabling users to log in, create accounts as employees or recruiters, search and apply for jobs. Implemented recruiter functionalities to post jobs, manage applicants, and review resumes with options to accept or reject candidates. Utilized JWT and cookies for secure authentication, Redux for efficient state management, Shadcn UI and Framer Motion for responsive and animated frontend. Built the backend using Node.js and Express, with MongoDB as the database for scalable data management.</p>
                         <div className='flex flex-wrap'>
                             <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>ReactJS</span>
                             <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>Cloudinary</span>
                             <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>ExpressJS</span>
                             <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>NodeJS</span>
                             <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>MongoDB</span>
+                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>ShadCN UI</span>
                         </div>
                     </motion.div>
                 </div>
