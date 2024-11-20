2c4c637eb881a7475c4dabf6b66324871caa68b8
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-11-20 15:06:10 +0530

    project delete

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,23 +11,6 @@ const Projects = () => {
             <div>
                 <div className='mb-8 flex flex-wrap lg:justify-center'>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1.5 }} className='w-full lg:w-1/4 mr-2'>
-                        <img src={project1} alt="" width={350} height={350} className='mb-6 rounded' />
-                    </motion.div>
-                    <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1.5 }} className='w-full max-w-xl lg:w-3/4'>
-                        <h6 className='mb-2 font-semibold'>Farm Fresh Products E-Commerce Platform - <a href="https://agri-boost-frontend.vercel.app/" target="_blank" rel="noopener noreferrer">AgriBoost</a></h6>
-                        <p className='mb-4 text-neutral-400'>Developed a dynamic e-commerce website for selling and showcasing farm fresh products with tailored farmer and admin dashboards. Implemented user authentication and authorization using JSON Web Tokens (JWT). Integrated Cloudinary for product image uploads and bcrypt for secure storage of user credentials. Utilized Axios package for seamless frontend-backend communication, updates and reviews. Designed a responsive interface with Bootstrap and React Icons, and managed data using MongoDB and ExpressJS.</p>
-                        <div className='flex flex-wrap'>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>MongoDB</span>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>ExpressJS</span>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>ReactJS</span>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>NodeJS</span>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>Bootstrap</span>
-                            <span className='mr-2 my-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>Cloudinary</span>
-                        </div>
-                    </motion.div>
-                </div>
-                <div className='mb-8 flex flex-wrap lg:justify-center'>
-                    <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: -100 }} transition={{ duration: 1.5 }} className='w-full lg:w-1/4 mr-2'>
                         <img src={project2} alt="" width={350} height={350} className='mb-6 rounded' />
                     </motion.div>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1.5 }} className='w-full max-w-xl lg:w-3/4'>
